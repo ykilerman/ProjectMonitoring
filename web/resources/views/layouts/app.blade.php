42dba0c0fe653bc5320d64d32d0b42d7465e65bc
Gradi <gradiph@gmail.com>
2016-08-10 13:06:22 +0700

    install bootstrap, jquery, and jquery-ui

--- web/resources/views/layouts/app.blade.php
+++ web/resources/views/layouts/app.blade.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<title>@yield('title')Project Monitoring App</title>
+    {{ Html::style(asset('css/bootstrap.min.css')) }}
+    {{ Html::style(asset('js/jquery-ui-1.12.0.custom/jquery-ui.min.css')) }}
+    {{ Html::script(asset('js/jquery-2.2.3.min.js')) }}
+    {{ Html::script(asset('js/bootstrap.min.js')) }}
+    {{ Html::script(asset('js/jquery-ui-1.12.0.custom/jquery-ui.min.js')) }}
 </head>
 <body>
 	<header>@include('layouts.header')</header>
