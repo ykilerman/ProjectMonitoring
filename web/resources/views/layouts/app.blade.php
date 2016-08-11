d42ef717bdfd575ae3f7d072a019c6ff52a9d3ed
Gradi <gradiph@gmail.com>
2016-08-11 11:03:09 +0700

    js function ajaxLoad

--- web/resources/views/layouts/app.blade.php
+++ web/resources/views/layouts/app.blade.php
@@ -10,10 +10,42 @@
     {{ Html::script(asset('js/jquery-2.2.3.min.js')) }}
     {{ Html::script(asset('js/bootstrap.min.js')) }}
     {{ Html::script(asset('js/jquery-ui-1.12.0.custom/jquery-ui.min.js')) }}
+	<style>
+	.loading {
+		background: lightgoldenrodyellow url('{{asset('images/processing.gif')}}') no-repeat center 65%;
+		height: 80px;
+		width: 100px;
+		position: fixed;
+		border-radius: 4px;
+		left: 50%;
+		top: 50%;
+		margin: -40px 0 0 -50px;
+		z-index: 2000;
+		display: none;
+	}
+    </style>
 </head>
 <body>
 	<header>@include('layouts.header')</header>
     @yield('content')
     <footer>@include('layouts.footer')</footer>
 </body>
+<script>
+function ajaxLoad(filename, content) {
+	content = typeof content !== 'undefined' ? content : 'content';
+	$('.loading').show();
+	$.ajax({
+		type: "GET",
+		url: filename,
+		contentType: false,
+		success: function (data) {
+			$("#" + content).html(data);
+			$('.loading').hide();
+		},
+		error: function (xhr, status, error) {
+			alert(xhr.responseText);
+		}
+	});
+}
+</script>
 </html>
